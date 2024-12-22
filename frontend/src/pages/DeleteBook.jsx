f612111bb39caee621e6f78ea1b9ed0b69f81184
Rahul Kumar <rahulkumarjha58978@gmail.com>
2024-12-22 20:18:16 +0530

    loading wlaa hata diye hai ab

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -6,7 +6,6 @@ import { useNavigate,useParams } from 'react-router-dom'
 
 function DeleteBook() {
 
-  const [loading,setloading]=useState(false);
   const Navigate=useNavigate();
   const {id}=useParams();
 
@@ -27,13 +26,10 @@ function DeleteBook() {
 
   const handleDeleteBook=async ()=>{
         try{
-          setloading(true);
           await axios.delete(`https://mern-book-blw9.onrender.com/books/${id}`);
-          setloading(false);
           Navigate('/');
         }
         catch(error){
-          setloading(false);
           alert("Error while calling delete api calling ",error);
           Navigate('/');
         }
@@ -43,7 +39,6 @@ function DeleteBook() {
     <div className='p-4'>
       <BackButton/>
       <h1 className='text-3xl my-4'>Delete Book</h1>
-      {loading?"loading.....":''}
       <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
         <h3 className='text-3xl'>Are You Sure You want to delete this book?</h3>
         <button className='p-4 bg-red-500 text-white m-8 w-full text-xl' onClick={handleDeleteBook}>
